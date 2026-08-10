5c2e6831e2b457f7cb207e31794171de8d2b6bc9
GREENboy0549 <mehdasohaib@gmail.com>
2026-08-10 16:12:32 +0100

    created nornir_config script

---
+++ automation/nornir_config.py
@@ -0,0 +1,22 @@
+import os
+from nornir import InitNornir
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_nornir():
+    return InitNornir(
+        runner={
+            "plugin": "threaded",
+            "options": {"num_workers": 10},
+        },
+        inventory={
+            "plugin": "NetBoxInventory2",
+            "options": {
+                "nb_url": os.getenv("NB_URL"),
+                "nb_token": os.getenv("NB_TOKEN"),
+                "ssl_verify": True,
+            },
+        },
+    )
