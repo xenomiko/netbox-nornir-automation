c264e398aee1c875c1e23f954509a190a6d23c6f
GREENboy0549 <mehdasohaib@gmail.com>
2026-09-26 20:50:19 +0100

    refactored the tests for cidr_to_netmask and added cidr_to_ip tests

--- automation/tests/test_renderer.py
+++ automation/tests/test_renderer.py
@@ -1,24 +1,68 @@
 import pytest
-from automation.renderer import cidr_to_netmask
+from automation.renderer import cidr_to_netmask, cidr_to_ip
 
 
 class TestCidrToNetmask:
-    def test_standard_ipv4_cidr(self):
-        assert cidr_to_netmask("10.0.0.1/24") == "10.0.0.1 255.255.255.0"
-
-    def test_cidr_is_zero(self):
-        assert cidr_to_netmask("10.0.0.1/0") == "10.0.0.1 0.0.0.0"
-
-    def test_cidr_is_32(self):
-        assert cidr_to_netmask("10.0.0.1/32") == "10.0.0.1 255.255.255.255"
-
-    def test_no_netmask(self):
-        assert cidr_to_netmask("10.0.0.1") == "10.0.0.1 255.255.255.255"
+    @pytest.mark.parametrize(
+        "input_cidr, expected",
+        [
+            ("10.0.0.1/24", "10.0.0.1 255.255.255.0"),
+            ("10.0.0.1/0", "10.0.0.1 0.0.0.0"),
+            ("10.0.0.1/32", "10.0.0.1 255.255.255.255"),
+            ("10.0.0.1", "10.0.0.1 255.255.255.255"),
+            ("3fff:172:20:20::2/64", "3fff:172:20:20::2 ffff:ffff:ffff:ffff::"),
+            (
+                "3fff:172:20:20::2/128",
+                "3fff:172:20:20::2 ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff",
+            ),
+            ("3fff:172:20:20::2/0", "3fff:172:20:20::2 ::"),
+        ],
+    )
+    def test_valid_cidr_to_netmask(self, input_cidr, expected):
+        assert cidr_to_netmask(input_cidr) == expected
 
     @pytest.mark.parametrize(
-        "invalid_ip", ["10.10.10.1/44", "10.10.10/32", "10.10.10/33", 33]
+        "invalid_ip", ["10.10.10.1/44", "10.10.10/32", "10.10.10/33", "hello", None]
     )
     def test_invalid_ip_cidr(self, invalid_ip):
-        ipaddress = invalid_ip
         with pytest.raises(ValueError):
-            cidr_to_netmask(ipaddress)
+            cidr_to_netmask(invalid_ip)
+
+
+class TestCidrToIp:
+    @pytest.mark.parametrize(
+        "input_ip, output_ip",
+        [
+            ("10.10.10.10/24", "10.10.10.10"),
+            ("10.10.10.10/32", "10.10.10.10"),
+            ("10.10.10.10/0", "10.10.10.10"),
+            ("3fff:172:20:20::2/64", "3fff:172:20:20::2"),
+            ("3fff:172:20:20::2/128", "3fff:172:20:20::2"),
+        ],
+    )
+    def test_valid_cidr_to_ip(self, input_ip, output_ip):
+        assert cidr_to_ip(input_ip) == output_ip
+
+    @pytest.mark.parametrize(
+        "invalid_input, expected_output",
+        [
+            (None, "None"),
+            ("", ""),
+        ],
+    )
+    def test_no_slash_input_passes_through_unvalidated(
+        self, invalid_input, expected_output
+    ):
+        assert cidr_to_ip(invalid_input) == expected_output
+
+    @pytest.mark.parametrize(
+        "invalid_input",
+        [
+            "/",
+            "hello/32",
+            "10.10.10/33",
+        ],
+    )
+    def test_slash_present_but_malformed_raises(self, invalid_input):
+        with pytest.raises(ValueError):
+            cidr_to_ip(invalid_input)
